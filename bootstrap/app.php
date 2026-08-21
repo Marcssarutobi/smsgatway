bee15bd3be1605ce5fcb8a248af1ee56281c070c
hstkbj <soheholmes7@gmail.com>
2026-08-21 10:08:07 +0200

    Ajout des alias pour les middlewares 'abilities' et 'ability' dans la configuration de l'application.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'device.auth' => \App\Http\Middleware\DeviceAuth::class,
             'api.key' => \App\Http\Middleware\ApiKeyAuth::class,
+            'abilities'   => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
+            'ability'     => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
